9c29c12a513a60871a81fb9e239f1ffb17affe7e
Tu Nombre <tuemail@ejemplo.com>
2025-10-16 18:07:33 -0300

    integracion api

--- alpr/urls.py
+++ alpr/urls.py
@@ -1,4 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-from django.urls import path
-from .views import upload_view
-urlpatterns = [ path('upload/', upload_view, name='alpr-upload') ]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('alpr/', include('alpr.urls')),             # UI web
+    path('api/',  include('alpr.api_urls')),         # API REST
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
